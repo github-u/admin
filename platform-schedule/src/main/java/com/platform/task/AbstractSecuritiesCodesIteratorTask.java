8f8df6e43355f6bff47b1ac52327956cc2375b56
unknown <it006007>
2020-02-01 16:37:28 +0800

    Commit By Git-Helper

--- platform-schedule/src/main/java/com/platform/task/AbstractSecuritiesCodesIteratorTask.java
+++ platform-schedule/src/main/java/com/platform/task/AbstractSecuritiesCodesIteratorTask.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.RateLimiter;
 import com.platform.entity.ResultSupport;
 import com.platform.jobx.domain.SimpleTaskParam;
 import com.platform.service.SecuritiesService;
@@ -12,17 +16,28 @@ import com.platform.utils.LangUtil;
 
 public abstract class AbstractSecuritiesCodesIteratorTask extends AbstractSecuritiesTask{
 	
-	//private static Logger logger = LoggerFactory.getLogger(SecuritiesCodesTask.class);
+	private static Logger logger = LoggerFactory.getLogger(SecuritiesCodesTask.class);
 	
 	@Resource
 	private SecuritiesService securitiesService;
 	
+	private RateLimiter rateLimiter = RateLimiter.create(Double.MAX_VALUE);
+			
 	@Override
 	public ResultSupport<String> process(SimpleTaskParam taskParam, Map<String, String> argMap) {
 		
 		ResultSupport<String> ret = new ResultSupport<String>();
 		
 		String paramSecuritiesCodes = LangUtil.safeString(argMap.get("securitiesCodes"));
+		String queryPerSeconds = LangUtil.safeString(argMap.get("QPS"));
+		try {
+			rateLimiter.setRate(Double.parseDouble(queryPerSeconds));
+		}catch(Exception e) {
+			logger.error("title=" + "AbstractSecuritiesCodesIteratorTask"
+					+ "$mode=" + "process"
+					+ "$errCode=" + "RATE_LIMITER_SET_EXCEPTION", e);
+			rateLimiter.setRate(Double.MAX_VALUE);
+		}
 		
 		ResultSupport<List<String>> securitiesCodesRet = null; 
 		if(paramSecuritiesCodes != null) {
@@ -38,6 +53,7 @@ public abstract class AbstractSecuritiesCodesIteratorTask extends AbstractSecuri
 		
 		Boolean interrupt = LangUtil.safeBoolean(argMap.get("interrupt"));
 		for(String securitiesCode : securitiesCodesRet.getModel()) {
+			rateLimiter.acquire();
 			ResultSupport<String> processRet = process(securitiesCode, taskParam, argMap);
 			if(!processRet.isSuccess() && interrupt) {
 				return ret.fail(processRet.getErrCode(), processRet.getErrMsg());
